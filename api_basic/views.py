43db24a75b1d85cadbe3bbe2c9e9b6ab33036ca4
tutorial <abhicse002@gmail.com>
2020-09-09 18:08:06 +0530

    Mkaing Class ArticleAPIView

--- api_basic/views.py
+++ api_basic/views.py
@@ -9,6 +9,26 @@ from rest_framework.response import Response
 from .serializers import ArticleSerializer
 from rest_framework import status
 
+from rest_framework.views import APIView
+
+
+class ArticleAPIView (APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many =True)
+        return Response(serializer.data)
+
+    def post(seld,request):
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid():
+            #if serializer is valid then only save
+            serializer.save()
+            #create status
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+
 @api_view(['GET' , 'POST'])
 def article_list(request):
     if request.method =='GET':
@@ -28,7 +48,33 @@ def article_list(request):
         return JsonResponse(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
         
 
+class ArticleDetails(APIView):
+    def get_object(self,id):
+        try:
+            return Article.objects.get(id = id)
+        except Article.DoesNotExist:
+            return Response(status = status.HTTP_404_NOT_FOUND)
 
+    def get(self , request , id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self , request , id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data = request.data)
+        if serializer.is_valid():
+            #if serializer is valid then only save
+            serializer.save()
+            #create status
+            return Response(serializer.data) 
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self , request , id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 #function based API view
 # @csrf_exempt
